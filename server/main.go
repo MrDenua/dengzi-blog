0e768cdc3dbab90ad48884f20ebd8524029c6db3
dengzi <denua@foxmail.com>
2019-11-27 18:06:06 +0800

    init project struct

--- server/main.go
+++ server/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
 )
 
 type MainController struct {
@@ -9,10 +11,27 @@ type MainController struct {
 }
 
 func (this *MainController) Get() {
-	this.Ctx.WriteString("hello world")
+
+	request := this.Ctx.Request
+	fmt.Println(request.Proto, request.Method, request.Host, request.URL.RawPath)
+	this.Ctx.WriteString("<h1>Hello World</h1>")
+}
+
+type RouterX struct {
+	beego.ControllerInterface
+}
+
+func (this RouterX) Init(ct *context.Context, controllerName, actionName string, app interface{}) {
+	fmt.Println(ct.Request.URL)
+	fmt.Println(controllerName, actionName)
+}
+
+func (this RouterX) Get() {
+	fmt.Println("GETTTf")
 }
 
 func main() {
+	beego.AutoRouter(&RouterX{})
 	beego.Router("/", &MainController{})
 	beego.Run()
 }
--- server/misc/goroutine.go
+++ server/misc/goroutine.go
@@ -1,4 +1,4 @@
-package main
+package misc
 
 func main() {
 
