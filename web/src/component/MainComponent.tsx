29d5c635014e939eb9c2fac44e90e3e763050707
dengzi <dengzii@foxmail.com>
2020-03-30 18:06:00 +0800

    update article list

--- web/src/component/MainComponent.tsx
+++ web/src/component/MainComponent.tsx
@@ -1,12 +1,9 @@
 import React, {useState} from 'react'
 import {BrowserRouter} from "react-router-dom";
-import Drawer from "@material-ui/core/Drawer";
-import DrawerComponent from "./DrawerComponent";
-import {Grid} from "@material-ui/core";
 import Box from "@material-ui/core/Box";
-import NavBarComponent from "./NavBarComponent";
+import {Grid, Paper} from "@material-ui/core";
 import MainRouter from "../router/MainRouter";
-import Hidden from "@material-ui/core/Hidden";
+import Container from "@material-ui/core/Container";
 
 function isMobile(): boolean {
     let mobileAgent = false;///Android|iPhone|iPod|BlackBerry/i.test(navigator.userAgent);
@@ -18,29 +15,44 @@ export default function MainComponent() {
 
     let [drawerHidden, setDrawerHidden] = useState(isMobile());
 
-    return (<div className="App">
+    return (<div className="App" >
+
         <BrowserRouter>
-            <Hidden mdUp>
-                <Drawer open={!drawerHidden && isMobile()} anchor={"left"}
-                        onClose={() => setDrawerHidden(!drawerHidden)}>
-                    <DrawerComponent/>
-                </Drawer>
-            </Hidden>
-            <Grid container spacing={0}>
-                <Grid item={true} md={2} hidden={drawerHidden}>
-                    <Hidden xsDown>
-                        <DrawerComponent/>
-                    </Hidden>
+            <Grid container style={{background: '#eee'}} justify={"center"}>
+                <Box minHeight={"60px"} width={"100%"} bgcolor={"white"} zIndex={999} position={"fixed"}>
+
+                </Box>
+                <Grid md={12} style={{background:"white"}}>
+
                 </Grid>
-                <Grid item={true} xs md xl lg>
-                    <Box height={"100vh"} width={"100%"} bgcolor="background.default">
-                        <NavBarComponent onMenuClick={setDrawerHidden} drawerHidden={drawerHidden}/>
-                        <Box style={{overflow: "auto"}}>
-                            <MainRouter/>
-                        </Box>
-                    </Box>
+                <Grid md={6} >
+                    <Box height={"16px"}/>
+                    <Paper elevation={0}>
+                        <MainRouter/>
+                    </Paper>
                 </Grid>
             </Grid>
+            {/*<Hidden mdUp>*/}
+            {/*    <Drawer open={!drawerHidden && isMobile()} anchor={"left"}*/}
+            {/*            onClose={() => setDrawerHidden(!drawerHidden)}>*/}
+            {/*        <DrawerComponent/>*/}
+            {/*    </Drawer>*/}
+            {/*</Hidden>*/}
+            {/*<Grid container spacing={0}>*/}
+            {/*    <Grid item={true} md={2} hidden={drawerHidden}>*/}
+            {/*        <Hidden xsDown>*/}
+            {/*            <DrawerComponent/>*/}
+            {/*        </Hidden>*/}
+            {/*    </Grid>*/}
+            {/*    <Grid item={true} xs md xl lg>*/}
+            {/*        <Box height={"100vh"} width={"100%"} bgcolor="background.default">*/}
+            {/*            <NavBarComponent onMenuClick={setDrawerHidden} drawerHidden={drawerHidden}/>*/}
+            {/*            <Box style={{overflow: "auto"}}>*/}
+            {/*                <MainRouter/>*/}
+            {/*            </Box>*/}
+            {/*        </Box>*/}
+            {/*    </Grid>*/}
+            {/*</Grid>*/}
         </BrowserRouter>
     </div>)
 }
